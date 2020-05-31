d26b9e1795ebd4154f5af51dac6477af1dd569b1
Sach <admin@sach.co.za>
2020-05-31 22:35:32 +0200

    added getters and setters for the number range class

--- src/numberrangesummarizer/NumberRange.java
+++ src/numberrangesummarizer/NumberRange.java
@@ -20,6 +20,34 @@ public class NumberRange {
 		this.end = end;
 	}
 
+	/**
+	 * @return the start
+	 */
+	public Integer getStart() {
+		return start;
+	}
+
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(Integer start) {
+		this.start = start;
+	}
+
+	/**
+	 * @return the end
+	 */
+	public Integer getEnd() {
+		return end;
+	}
+
+	/**
+	 * @param end the end to set
+	 */
+	public void setEnd(Integer end) {
+		this.end = end;
+	}
+
 	@Override
 	public String toString() {
 		return start + "-" + end;
